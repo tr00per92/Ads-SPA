a1141f8ba3a0063642410b45e83ce0f312d5fac9
tr00per92 <zvezdo@mail.bg>
2014-12-29 17:09:39 +0200

    implemented town and category filtering

--- scripts/controllers/HomeCtrl.js
+++ scripts/controllers/HomeCtrl.js
@@ -2,13 +2,16 @@ define(['app', 'services/adData', 'services/otherData', 'directives/realSrc'], f
     app.controller('HomeCtrl', function ($scope, $rootScope, adData, otherData) {
         $rootScope.title = 'Ads - Home';
 
+        $scope.selectedTown = '';
+        $scope.selectedCategory = '';
+        $scope.selectedPage = 1;
         $scope.loadAds = function () {
-            adData.getAds($scope.selectedPage).then(function (data) {
-                $scope.totalAds = data.numItems;
-                $scope.ads = data.ads;
+            adData.getAds($scope.selectedPage, $scope.selectedTown, $scope.selectedCategory)
+                .then(function (data) {
+                    $scope.totalAds = data.numItems;
+                    $scope.ads = data.ads;
             });
         };
-        $scope.selectedPage = 1;
         $scope.loadAds();
 
         otherData.getCategories().then(function (data) {
@@ -17,5 +20,18 @@ define(['app', 'services/adData', 'services/otherData', 'directives/realSrc'], f
         otherData.getTowns().then(function (data) {
             $scope.towns = data;
         });
+
+        $scope.townChanged = function (townId) {
+            if ($scope.selectedTown !== townId) {
+                $scope.selectedTown = townId;
+                $scope.loadAds();
+            }
+        };
+        $scope.categoryChanged = function (categoryId) {
+            if ($scope.selectedCategory !== categoryId) {
+                $scope.selectedCategory = categoryId;
+                $scope.loadAds();
+            }
+        };
     });
 });
--- scripts/services/adData.js
+++ scripts/services/adData.js
@@ -2,7 +2,7 @@ define(['app'], function (app) {
     app.factory('adData', function ($http, $q, backendUrl) {
         function getAds(startPage, townId, categoryId) {
             var deferred = $q.defer();
-            $http.get(backendUrl + 'ads?pagesize=4&startpage=' + startPage)
+            $http.get(backendUrl + 'ads?pagesize=4&startpage=' + startPage + '&townid=' + townId + '&categoryid=' + categoryId)
                 .success(function (data) {
                     deferred.resolve(data);
                 })
